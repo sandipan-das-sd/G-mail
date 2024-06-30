8d85f4c46369f3951259562ca12657041a7ec5e5
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-06-30 19:21:52 +0530

    updated

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -5,7 +5,7 @@ import { MdCropSquare } from 'react-icons/md'
 
 export default function Message() {
   return (
-    <div className='flex items-start justify-between border-gray-200 px-4 text-sm hover:cursor-pointer hover:shadow:md '>
+    <div className='flex items-start  justify-between border-gray-200 px-4 py-3 text-sm hover:cursor-pointer hover:shadow-md'>
       <div className='flex items-center gap-3' >
 
         <div className='flex text-gray-300'>
@@ -17,8 +17,12 @@ export default function Message() {
         </div>
       </div>
       <div className=" flex-1 ml-4">
-        <p className="" >Lorem dsjfjafjsdfijfif jfsjsfjfjfn jjkkjgguuhu hhjfffffyyy ftdrrrdttt iuugddrddd</p>
+        <p className="text-gray-600 truncate inline-block max-w-full">Lorem dsjfjafjsdfijfif jfsjsfjfjfn jjkkjgguuhu hhjfffffyyy ftdrrrdttt iuugddrddd</p>
 
+      </div>
+
+      <div className=" flex-none text-gray-400 text-sm">
+        Time
       </div>
     </div>
   )
