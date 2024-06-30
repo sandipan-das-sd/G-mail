74cc04f50901489af4d6cd88646a4b1b1f9b7986
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-01 00:12:29 +0530

    updated

--- client/src/Mail.js
+++ client/src/Mail.js
@@ -1,9 +1,87 @@
-import React from 'react'
+import React from 'react';
+import { IoMdMore, IoMdArrowBack } from "react-icons/io";
+import {
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+  MdDeleteOutline,
+  MdOutlineReport,
+  MdOutlineMarkEmailUnread,
+  MdOutlineWatchLater,
+  MdOutlineAddTask,
+  MdOutlineDriveFileMove,
+} from "react-icons/md";
+import { BiArchiveIn } from "react-icons/bi";
+import { useNavigate } from 'react-router-dom';
 
 export default function Mail() {
+  const navigate=useNavigate()
   return (
-    <div>
-      Mail
+    <div className='flex-1 bg-white rounded-xl mx-5 '>
+      <div className='flex items-center justify-between px-4'>
+        <div className='flex items-center gap-2 text-gray-700 py-2'>
+          <div onClick={()=>{
+            navigate('/')
+          }} className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <IoMdArrowBack size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <MdKeyboardArrowLeft size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <MdKeyboardArrowRight size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <MdDeleteOutline size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <MdOutlineReport size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <MdOutlineMarkEmailUnread size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <MdOutlineWatchLater size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <MdOutlineAddTask size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <MdOutlineDriveFileMove size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <BiArchiveIn size={'20px'} />
+          </div>
+          <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <IoMdMore size={'20px'} />
+          </div>
+          <div className='flex items-center gap-2'>
+            <button className='hover:rounded-full hover:bg-gray-100'>
+              <MdKeyboardArrowLeft size={'20px'} />
+            </button>
+            <button className='hover:rounded-full hover:bg-gray-100'>
+              <MdKeyboardArrowRight size={'20px'} />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='h-[90vh] overflow-y-auto p-4'>
+      <div className='flex items-center justify-between bg-white gap-1 '>
+        <div className='flex items-center gap-2'>
+      <h1 className='text-xl font-medium'> Subject</h1>
+      <span className='text-sm bg-gray-200 rounded-md px-2'> Inbox</span>
+        </div>
+        <div className='flex-none text-gray-400 my-5 text-sm '>
+          <p>26-04-2024</p>
+        </div>
+        </div>
+        <div className='text-gray-500 text-sm'>
+          <h1>sd901656@gmail.com</h1>
+          <span> to me</span>
+        <div className='my-10'>
+          <p> Messages nsnddfjffjffkwfkfff</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,11 +1,16 @@
 import { RiStarSLine } from "react-icons/ri";
-
+import { useNavigate } from "react-router-dom";
 import React from 'react'
 import { MdCropSquare } from 'react-icons/md'
 
 export default function Message() {
+  const navigate=  useNavigate();
+  const openMail=()=>{
+   
+    navigate('/mail/626655655')
+  }
   return (
-    <div className='flex items-start  justify-between border-gray-200 px-4 py-3 text-sm hover:cursor-pointer hover:shadow-md'>
+    <div onClick={openMail} className='flex items-start  justify-between border-gray-200 px-4 py-3 text-sm hover:cursor-pointer hover:shadow-md'>
       <div className='flex items-center gap-3' >
 
         <div className='flex text-gray-300'>
