a599b1df36a45600707b3c31ae4fdf10acc12a46
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-06-30 08:33:17 +0530

    routing updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import Body from './components/Body';
+import Inbox from './components/Inbox';
 import Navbar from './components/shared/Navbar';
-import Sidebar from './components/Sidebar';
 
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Mail from './Mail';
+const router= createBrowserRouter(
+[
+  {
+    path:'/',
+    element:<Body/>,
+    //through outlet children data passed
+    children:[
+      {
+        path:'/',
+        element:<Inbox/>
+      },
+      {
+        path:'/mail/:id',
+        element:<Mail/>
+      },
+     
+    ]
+  }
+]
+)
 function App() {
   return (
     <div className='bg-[#F6F8FC] h-screen w-screen overflow-hidden '>
       <Navbar />
-      <Sidebar/>
+      <RouterProvider router={router}/>
+      
     </div>
   );
 }
---
+++ client/src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import { Outlet } from 'react-router-dom'
+
+export default function Body() {
+  return (
+    <div className='flex'>
+     <Sidebar/>
+     <Outlet/>
+    </div>
+  )
+}
